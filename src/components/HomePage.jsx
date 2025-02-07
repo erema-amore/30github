7f782d309e7f7f273479f657f1365eba9acfea51
erema-amore <erema.amore@mail.ru>
2025-02-07 11:49:26 +0600

    3

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './HomePage.css'
 
 const HomePage = () => {
   return (
     <div>
-      
+      <h2 className='text'>My experions with growthhungry!</h2>
     </div>
   );
 };
