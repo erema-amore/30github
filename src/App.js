cc25bb8bf3426a0586cd6b9f8aca73d0f88b73c5
erlan-tarielov-dev <erlan.tarielov.dev@gmail.com>
2025-03-05 09:07:20 +0600

    create AddToList component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import AuthStatus from './components/AuthStatus';
+import AddToList from './components/AddToList';
+
 
 
 
@@ -8,7 +9,7 @@ function App() {
     
       <div className="App">
        
-        <AuthStatus/>
+        <AddToList/>
       </div>
     
   );
